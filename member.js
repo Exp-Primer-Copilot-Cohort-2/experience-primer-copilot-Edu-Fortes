de260c25fe79840abbcce153377c7e13e486fd7f
Eduardo Morikuni Fortes <46508709+Edu-Fortes@users.noreply.github.com>
2024-08-02 04:11:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        firstName: "John",
+        lastName: "Doe",
+        skills: ["JavaScript", "HTML", "CSS"],
+        age: 25
+    };
+    return member;
+}
